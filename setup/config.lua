28d4da6922de5b81ea4aedd2c193fe8a32662abd
Ghostmode65 <65222481+Ghostmode65@users.noreply.github.com>
2025-12-24 16:52:27 -0600

    config update

--- setup/config.lua
+++ setup/config.lua
@@ -1,17 +1,19 @@
 local ScriptTrigger = Reflection:getClass("xyz.wagyourtail.jsmacros.core.config.ScriptTrigger")
 local TriggerType = Reflection:getClass("xyz.wagyourtail.jsmacros.core.config.ScriptTrigger$TriggerType")
 
+local file = FS:open("unified/loader/jLoader.lua"):getFile()
+    if not file then return nil end
+
 local trigger = Reflection:newInstance(
     ScriptTrigger,
     {
-        TriggerType.event,
+        TriggerType.EVENT,
         "LaunchGame",
-        FS:open("unified/loader/jLoader.lua"):getFile(),
+        file,
         true,
         false
     }
 )
 JsMacros:getProfile():getRegistry():addScriptTrigger(trigger);
 
---need to check if jLoader exists otherwise it throws error
 --Remove installer trigger after setup
