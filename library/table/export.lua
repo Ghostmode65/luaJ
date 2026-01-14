6a2033d25c825810e331786c0c848eadcd248da5
Ghostmode65 <65222481+Ghostmode65@users.noreply.github.com>
2026-01-14 01:28:12 -0600

    filename to filepath

--- library/table/export.lua
+++ library/table/export.lua
@@ -1,15 +1,15 @@
-LuaJ.saveTable = function(filename,folder,Table) --.roaming/.JsMacros/
+LuaJ.saveTable = function(filepath,Table) --.roaming/.JsMacros/
     if not Serpent then return nil end
-    if not folder then folder = "" end
-        if folder ~= "" and not folder:find("/$") then folder = folder .. "/" end
-        folder = LuaJ.directory.roaming[".jsMacros"]..folder
-       
-        if not filename:match("%.lua$") then filename = filename .. ".lua" end
-        local file = folder..filename
 
+    local folder = LuaJ.directory.roaming[".jsMacros"]..(filepath:match("(.*/)") or "")
+        
+    if not filepath:match("%.lua$") then filepath = filepath .. ".lua" end
+    local file = LuaJ.directory.roaming[".jsMacros"]..filepath
+        
     --if disableOverwrite and FS:exists(file) then return false end
     if not FS:exists(file) then FS:createFile(folder,file,true) Client:waitTick() end
         local content = Serpent.dump(Table)
         FS:open(file):write(content)
     return true
 end
+
