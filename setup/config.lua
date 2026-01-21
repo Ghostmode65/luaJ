9cb06bb629005178b60240ab182fc8c2777de74f
Ghostmode65 <65222481+Ghostmode65@users.noreply.github.com>
2026-01-21 13:11:15 -0600

    Test 7

--- setup/config.lua
+++ setup/config.lua
@@ -21,12 +21,12 @@ if not hasjLoader() then LuaJ.addScriptTrigger("jLoader.lua","event","LaunchGame
 for i,v in pairs(LuaJ.setup["Keybinds"]) do
     if v.url == "" then return nil end
 
-    local filename = Import.download(v.url,"scripts/macros/"..(v.folder or ""))
-
     if v.folder then
         if v.folder:sub(-1) ~= "/" then v.folder = v.folder .. "/" end
         if v:match("%.lua$") then Chat:log("§cKeybinds: "..tostring(v.folder).. " is not a valid folder name.") return false end
     end
+
+    local filename = Import.download(v.url,"scripts/macros/"..(v.folder or ""))
 
     local dir = "unified/"..(v.folder and (v.folder.."/"..filename) or filename)
     LuaJ.addScriptTrigger(dir,v.event or "keydown",v.key)
